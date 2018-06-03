aa488c7aa1441963b21da97aec0e6aba604f7c1d
Lynn <Lynn@LAPTOP-635O7HE9>
2018-06-03 12:08:13 +0800

    LessonFive Demonstration

---
+++ src/test/java/com/bbc/LessonFive/mvnTestNG.java
@@ -0,0 +1,13 @@
+package com.bbc.LessonFive;
+
+import org.testng.annotations.Test;
+
+public class mvnTestNG 
+{
+	@Test
+	public void HelloWorld()
+	{
+		System.out.println("Hello World!");
+	}
+
+}
